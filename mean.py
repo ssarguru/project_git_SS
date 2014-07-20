0fdeab53d9410443f37444cc6e47de25d03b9745
Sarguru Subash <sargurumsu@gmail.com>
2014-07-20 10:24:04 -0400

    changes to big_file

--- mean.py
+++ mean.py
@@ -1,10 +1,13 @@
 "mean.py"
+#print the mean
+#changes to big_file
+
 import sys
 
 #sum input values
 sum = 0
 n = 0
-for num in sys.stdin:
+for num in open('data.txt'):
 	sum += float(num)
 	n += 1
 print sum / n
